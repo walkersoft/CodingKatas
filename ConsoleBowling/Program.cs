b22913697342e676315b3e77233f57cf92805228
Jason L. Walker <jasonlanewalker@gmail.com>
2021-04-28 22:00:10 -0500

    working on displaying the scoreboard and string formatting

--- ConsoleBowling/Program.cs
+++ ConsoleBowling/Program.cs
@@ -5,10 +5,27 @@ var scorer = new ScoringCalculator();
 int[] rolls = { 2, 4 };
 int[] frames = scorer.CalculateScore(rolls);
 
+//game state vars
+string[] displayFrames = new string[11];
+string[] displayRolls = new string[21];
+int[] gameFrames;
+int currentRollScore;
 
-DrawScoreboard();
+for (int i = 0; i < displayRolls.GetLength(0); i++)
+{
+    displayRolls[i] = "0";
+}
 
-static void DrawScoreboard()
+for (int i = 0; i < displayFrames.Length; i++)
+{
+    displayFrames[i] = "0";
+}
+
+DrawScoreboard(displayRolls, displayFrames);
+Console.Write("Next score: ");
+Console.ReadKey();
+
+static void DrawScoreboard(string[] displayRolls, string[] displayFrames)
 {
     DrawBorder();
     Console.Write("|");
@@ -20,6 +37,26 @@ static void DrawScoreboard()
 
     Console.WriteLine();
     DrawBorder();
+    DrawScores(displayRolls, displayFrames);
+    DrawBorder();
+}
+
+static void DrawScores(string[] displayRolls, string[] displayFrames)
+{
+    Console.Write("|");
+    for (int i = 0; i < 9; i++)
+    {
+        Console.Write(string.Format("{0} {1} |", displayRolls[i], displayRolls[i + 1]));
+    }
+    Console.Write(string.Format("{0} {1}{2}|", displayRolls[18], displayRolls[19], displayRolls[20]));
+    Console.WriteLine();
+    DrawBorder();
+    Console.Write("|");
+    for (int i = 0; i < 10; i++)
+    {
+        Console.Write(string.Format(" {0,3}|", displayFrames[i]));
+    }
+    Console.WriteLine();
 }
 
 static void DrawBorder()
