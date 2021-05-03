c9534b65b04290b6d272666715aff8b51f0d2bce
Jason L. Walker <jasonlanewalker@gmail.com>
2021-05-03 17:45:38 -0500

    played a game of bowling with correct score and scoreboard markings

--- ConsoleBowling/Program.cs
+++ ConsoleBowling/Program.cs
@@ -11,6 +11,7 @@ int displayFrameRollsIndex = 0;
 string[] displayFrames = new string[11];
 List<int> gameRolls = new();
 int ballIndex = 0;
+int[] frames = new int[11];
 
 
 for (int i = 0; i < displayFrameRolls.GetLength(0); i++)
@@ -26,58 +27,99 @@ for (int i = 0; i < displayFrames.Length; i++)
     displayFrames[i] = "0";
 }
 
+
+
 do
 {
     Console.SetCursorPosition(0, 0);
     DrawScoreboard(gameRolls.ToArray(), displayFrameRolls, displayFrames);
+
+    if (ballIndex == 3)
+    {
+        break;
+    }
+
     int inputScore = GetInput();
 
 
     gameRolls.Add(inputScore);
-    var frames = scorer.CalculateScore(gameRolls.ToArray());
+    frames = scorer.CalculateScore(gameRolls.ToArray());
 
     for (int i = 0; i < frames.Length; i++)
     {
         displayFrames[i] = frames[i].ToString();
     }
 
-    if (ballIndex == 0)
+    if (displayFrameRollsIndex < 9)
     {
-        if (scorer.LastScoreWasStrike)
+        if (ballIndex == 0)
+        {
+            if (scorer.LastScoreWasStrike)
+            {
+                displayFrameRolls[displayFrameRollsIndex, ballIndex] = "X";
+                displayFrameRolls[displayFrameRollsIndex, ballIndex + 1] = " ";
+                displayFrameRollsIndex++;
+                continue;
+            }
+
+            if (displayFrameRollsIndex > 9)
+            {
+                displayFrameRollsIndex = 9;
+            }
+
+            displayFrameRolls[displayFrameRollsIndex, ballIndex] = inputScore.ToString();
+        }
+
+        if (ballIndex == 1)
+        {
+            if (scorer.LastScoreWasSpare)
+            {
+                displayFrameRolls[displayFrameRollsIndex, ballIndex] = "/";
+            }
+            else
+            {
+                displayFrameRolls[displayFrameRollsIndex, ballIndex] = inputScore.ToString();
+            }
+
+            if (displayFrameRollsIndex < 9)
+            {
+                displayFrameRollsIndex++;
+                ballIndex--;
+            }
+
+            continue;
+        }
+
+        ballIndex++;
+        continue;
+    }
+
+    if (displayFrameRollsIndex == 9)
+    {
+
+        if (inputScore == 10)
         {
             displayFrameRolls[displayFrameRollsIndex, ballIndex] = "X";
-            displayFrameRolls[displayFrameRollsIndex, ballIndex + 1] = " ";
-            displayFrameRollsIndex++;
+            ballIndex++;
+            continue;
+        }
+
+        if ((ballIndex == 1 || ballIndex == 2) && (inputScore + gameRolls[^2] == 10))
+        {
+            displayFrameRolls[displayFrameRollsIndex, ballIndex] = "/";
+            ballIndex++;
             continue;
         }
 
         displayFrameRolls[displayFrameRollsIndex, ballIndex] = inputScore.ToString();
-    }
-    
-    if (ballIndex == 1)
-    {
-        if (scorer.LastScoreWasSpare)
-        {
-            displayFrameRolls[displayFrameRollsIndex, ballIndex] = "/";
-        }
-        else
-        {
-            displayFrameRolls[displayFrameRollsIndex, ballIndex] = inputScore.ToString();
-        }
-    
-        if (displayFrameRollsIndex < 9)
-        {
-            displayFrameRollsIndex++;
-            ballIndex--;
-        }
 
-        continue;
-    }
+        ballIndex++;      
 
-    ballIndex++;
-    
+    }    
 }
 while (true);
+
+Console.WriteLine(string.Format("Game concluded. Total Score: {0}", frames[10]));
 
 static void DrawScoreboard(int[] gameRolls, string[,] displayRolls, string[] displayFrames)
 {
